0c385cc5fdafad265d3169d532eaf84c2be6a2a2
Tyler Filla <tylerfilla@hotmail.com>
2018-05-11 10:13:26 -0500

    boilerplates

--- prog1.cpp
+++ prog1.cpp
@@ -3,7 +3,24 @@
  * OPC18 - Problem 1
  */
 
+#include <algorithm>
+#include <cstdlib>
+#include <iterator>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
 int main(int argc, char* argv[])
 {
+    if (argc == 0)
+    {
+        // no arguments
+        return 1;
+    }
+
+    std::cout << "Welcome to program 1!\n";
+
     return 0;
 }
--- prog5.cpp
+++ prog5.cpp
@@ -3,7 +3,24 @@
  * OPC18 - Problem 5
  */
 
+#include <algorithm>
+#include <cstdlib>
+#include <iterator>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
 int main(int argc, char* argv[])
 {
+    if (argc == 0)
+    {
+        // no arguments
+        return 1;
+    }
+
+    std::cout << "Welcome to program 5!\n";
+
     return 0;
 }
